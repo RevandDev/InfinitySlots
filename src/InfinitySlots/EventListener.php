9dfd7c1d9863c98973f82fae57f179167128187b
Revand Mika <82688238+RevandDev@users.noreply.github.com>
2022-03-13 18:55:42 +0800

    Added PlayerPreLoginEvent
    Make Player Can Join If Server Max Players Is Full

--- src/InfinitySlots/EventListener.php
+++ src/InfinitySlots/EventListener.php
@@ -4,10 +4,17 @@ namespace InfinitySlots;
 
 use pocketmine\event\Listener;
 use pocketmine\event\server\QueryRegenerateEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
 
 class EventListener implements Listener{
     
     public function onQueryRegenerate(QueryRegenerateEvent $event){
         $event->getQueryInfo()->setMaxPlayerCount($event->getQueryInfo()->getPlayerCount() + 1);
+    }
+    
+    public functipn onPreLogin(PlayerPreLoginEvent $event){
+        if(in_array($event::KICK_REASON_SERVER_FULL, $event->getKickReasons())){
+            $event->uncancel();
+        }
     }
 }
